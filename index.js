5aebd582d6af7849edfde49505eb8f009b85d2e5
hoodietre <hoodietre@gmail.com>
2026-03-26 18:45:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ if (interaction.commandName === 'assignrole') {
   console.log(`Assigned role ${role.id} to member ${targetMember.id}`);
   return;
 }
-}  } catch (error) {
+} catch (error) {
     console.error('Interaction handler error:', error);
 
     try {
