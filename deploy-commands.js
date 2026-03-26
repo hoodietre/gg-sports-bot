54bae13b4bb97d263701d76d4fbd1afb5e51c455
hoodietre <hoodietre@gmail.com>
2026-03-26 14:59:10 -0400

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,13 +1,29 @@
-import { REST, Routes } from 'discord.js';
+import { REST, Routes, SlashCommandBuilder } from 'discord.js';
 
 const CLIENT_ID = '1407760487151833200';
 const GUILD_ID = '1486545386649686068';
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Check if bot is working'),
+
+  new SlashCommandBuilder()
+    .setName('whogotnext')
+    .setDescription('Notify the league you are ready to play')
+    .addStringOption(option =>
+      option
+        .setName('message')
+        .setDescription('Optional extra message')
+        .setRequired(false)
+    ),
+].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 await rest.put(
   Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-  { body: [] }
+  { body: commands }
 );
 
-console.log('Commands cleared.');
+console.log('Commands deployed.');
