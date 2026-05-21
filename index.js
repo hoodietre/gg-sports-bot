137da920030c03564ddb93db6b469970d09ae5d6
hoodietre <hoodietre@gmail.com>
2026-05-21 02:45:54 -0400

    Clear and re-register global commands in Discord bot

--- index.js
+++ index.js
@@ -1283,10 +1283,28 @@ function getRegisteredCommands() {
 
 async function registerCommands() {
   const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+
+  console.log('Clearing ALL guild commands...');
+  await rest.put(
+    Routes.applicationGuildCommands(CLIENT_ID, DEV_GUILD_ID),
+    { body: [] }
+  );
+
+  console.log('Clearing ALL global commands...');
+  await rest.put(
+    Routes.applicationCommands(CLIENT_ID),
+    { body: [] }
+  );
+
+  console.log('Registering fresh global commands...');
   const commands = getRegisteredCommands();
 
-  await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-  console.log('Global commands synced:', commands.length);
+  await rest.put(
+    Routes.applicationCommands(CLIENT_ID),
+    { body: commands }
+  );
+
+  console.log('Fresh global commands synced:', commands.length);
 }
 
 async function getLeagueByName(guildId, leagueName) {
